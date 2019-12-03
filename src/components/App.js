8dfd49c2ab7ddc31591f67a694d33a867c4d9673
rhysgoehring <rhysgoehring@gmail.com>
2019-12-03 15:48:49 -0700

    started writing TitleSearch component, UI is complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import TitleSearch from "./TitleSearch";
 import "./app.scss";
 
 class App extends Component {
@@ -7,9 +8,10 @@ class App extends Component {
     return (
       <div className="app">
         <header>
-          <Link to="/" alt="The Movie List">
+          <Link to="/" alt="The Movie List" className="title">
             <h1 className="title">The Movie List</h1>
           </Link>
+          <TitleSearch />
         </header>
 
         {this.props.children}
---
+++ src/components/TitleSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TitleSearch = props => (
+  <form className="title-search">
+    <input className="title-search__input" placeholder="Find a movie..."/>
+    <button className="title-search__button">Search</button>
+  </form>
+);
+
+export default TitleSearch;
