8925615fae0cf6c3b429787db122a6efb824f849
rhysgoehring <rhysgoehring@gmail.com>
2019-12-03 09:31:53 -0700

    was having issues with npm, switched package manager to yarn. installed dependencies and added redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
-import createHistory from 'history/createBrowserHistory';
-import { routerMiddleware } from 'react-router-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./rootReducer";
+import { createBrowserHistory } from "history";
+import { routerMiddleware } from "react-router-redux";
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 const historyMiddleware = routerMiddleware(history);
-const middleware = [thunk, historyMiddleware];
+const middleware = [thunk, historyMiddleware, logger];
 
-export default function configureStore(initialState={}) {
+export default function configureStore(initialState = {}) {
   return createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(...middleware),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getAllMovies } from './state/actions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getAllMovies } from "./state/actions";
 
 class Home extends Component {
   componentDidMount() {
-    this.props.getAllMovies()
+    console.log("home");
+    this.props.getAllMovies();
   }
 
   render() {
     return (
       <div>
         {this.props.movies.map(movie => (
-          <div>
-            {movie.title}
-          </div>
+          <div>{movie.title}</div>
         ))}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  movies: state.main.movies,
-})
+  movies: state.main.movies
+});
 
 const mapDispatchToProps = dispatch => ({
   getAllMovies: () => dispatch(getAllMovies())
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
