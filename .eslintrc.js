29eb2cd878a5d80023eceadd24cc2e71eddeab6b
rhysgoehring <rhysgoehring@gmail.com>
2019-12-03 09:42:45 -0700

    confiured eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    allowImportExportEverywhere: false,
+    codeFrame: false,
+  },
+  extends: ["airbnb", "prettier", "prettier/react", "plugin:jsx-a11y/recommended"],
+  plugins: ["react", "prettier", "react-hooks", "import", "jsx-a11y"],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  rules: {
+    "max-len": ["error", { code: 100 }],
+    // 'prefer-promise-reject-errors': ['off'],
+    "react/jsx-filename-extension": ["off"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    // 'no-return-assign': ['off'],
+    "react/prop-types": "off",
+  },
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import './app.scss';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./app.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <header>
-          <Link to="/" alt="The Movie List"><h1 className="title">The Movie List</h1></Link>
+          <Link to="/" alt="The Movie List">
+            <h1 className="title">The Movie List</h1>
+          </Link>
         </header>
 
         {this.props.children}
