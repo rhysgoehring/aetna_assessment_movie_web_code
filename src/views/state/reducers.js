cf2ab335bf687f791315438e14443496bd5fa916
rhysgoehring <rhysgoehring@gmail.com>
2019-12-03 12:23:58 -0700

    added object-rest-spread plugin to new .babelrc

--- src/views/state/reducers.js
+++ src/views/state/reducers.js
@@ -1,11 +1,13 @@
 const initialState = {
   movies: [],
+  selectedMovie: {},
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case "ALL_MOVIES_LOADED":
       return {
+        ...state,
         movies: action.payload.movies,
       };
     default:
