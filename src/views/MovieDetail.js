ae1cf707ef51569693e61f08d35aa8b9def4318f
rhysgoehring <rhysgoehring@gmail.com>
2019-12-03 14:18:31 -0700

    almost done with MovieDetail, need to add table

--- src/views/MovieDetail.js
+++ src/views/MovieDetail.js
@@ -5,12 +5,24 @@ import { getMovieDetails } from "./state/actions";
 class MovieDetail extends Component {
   componentDidMount() {
     const { imdbId } = this.props.location;
-    // console.log("imdbId", imdbId);
     this.props.getMovieDetails(imdbId);
   }
 
   render() {
-    return <div>Movie Detail</div>;
+    console.log(this.props.selectedMovie);
+    const { selectedMovie } = this.props;
+    return (
+      <div className="movie-detail">
+        <img className="movie-detail__image" src={selectedMovie.Poster} alt={selectedMovie.Title} />
+        <div className="movie-detail__info">
+          <div className="movie-detail__title">
+            <h2 className="movie-detail__heading">{selectedMovie.Title}</h2>
+            <h2 className="movie-detail__heading">{selectedMovie.imdbRating}/10</h2>
+          </div>
+          <p>{selectedMovie.Plot}</p>
+        </div>
+      </div>
+    );
   }
 }
 
