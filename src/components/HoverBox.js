2c9ae72bc6f5ca4a6e6fd5d296ee86bbb01fee0d
rhysgoehring <rhysgoehring@gmail.com>
2019-12-04 13:11:10 -0700

    created HoverBox, displaying when user hovers over a movie

---
+++ src/components/HoverBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const HoverBox = props => {
+  return (
+    <div className="hoverbox">Hoverbox</div>
+  )
+};
+
+export default HoverBox;
--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import HoverBox from "./HoverBox";
 
-const MovieItem = ({ movie }) => (
-  <Link
-    className="link"
-    to={{
-      pathname: `/${movie.id}`,
-      imdbId: movie.imdbId,
-    }}
-  >
-    <div className="movie-item">
-      <img className="movie-item__image" alt={movie.title} src={movie.poster} />
-      <p className="movie-item__title">{movie.title}</p>
+const MovieItem = ({ movie }) => {
+  const [hovering, setHovering] = useState(false);
+
+  const handleMouseOver = e => {
+    console.log("mouseOver e", movie);
+    setHovering(true);
+  };
+
+  return (
+    <div onFocus onMouseOver={handleMouseOver} onMouseLeave={() => setHovering(false)}>
+      <Link
+        className="link"
+        to={{
+          pathname: `/${movie.id}`,
+          imdbId: movie.imdbId,
+        }}
+      >
+        <div className="movie-item">
+          <img className="movie-item__image" alt={movie.title} src={movie.poster} />
+          <p className="movie-item__title">{movie.title}</p>
+          {hovering && <HoverBox />}
+        </div>
+      </Link>
     </div>
-  </Link>
-);
+  );
+};
 
 export default MovieItem;
